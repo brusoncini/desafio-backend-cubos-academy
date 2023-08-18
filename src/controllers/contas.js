759b8681e7fcf89816951a65034eb6f590374f81
Bruna Soncini <brusoncini@gmail.com>
2023-08-18 17:02:30 -0300

    funções put e delete

--- src/controllers/contas.js
+++ src/controllers/contas.js
@@ -1,12 +1,4 @@
-let {
-  banco,
-  numero,
-  saldo,
-  contas,
-  saques,
-  depositos,
-  transferencias,
-} = require("../bancodedados");
+let { numero, saldo, contas } = require("../bancodedados");
 
 const listarContas = (req, res) => {
   res.json(contas);
@@ -15,13 +7,22 @@ const listarContas = (req, res) => {
 const criarConta = (req, res) => {
   const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
 
+  // verificações
   if (!nome || !cpf || !data_nascimento || !telefone || !email || !senha) {
     return res
       .status(400)
-      .json({ mensagem: "Todas as informações são obrigatórias." })
+      .json({ mensagem: "Todas as informações são obrigatórias." });
   }
 
-  const conta = {
+  for (const conta of contas) {
+    if (conta.cpf === cpf || conta.email === email) {
+      return res.status(400).json({
+        mensagem: "Já existe uma conta com o cpf ou e-mail informado!",
+      });
+    }
+  }
+  // cria uma nova conta
+  const novaConta = {
     numero: numero++,
     saldo: 0,
     nome,
@@ -29,17 +30,88 @@ const criarConta = (req, res) => {
     data_nascimento,
     telefone,
     email,
-    senha
+    senha,
   };
 
-  contas.push(conta);
+  contas.push(novaConta);
 
   return res.status(201).send();
 };
 
-const atualizarConta = (req, res) => {};
+const atualizarConta = (req, res) => {
+  const { numeroConta, usuario } = req.params;
+  const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
 
-const excluirConta = (req, res) => {};
+  // verificações
+  if (!nome || !cpf || !data_nascimento || !telefone || !email || !senha) {
+    return res
+      .status(400)
+      .json({ mensagem: "Todas as informações são obrigatórias." });
+  }
+
+  for (const conta of contas) {
+    if (conta.cpf === cpf || conta.email === email) {
+      return res.status(400).json({
+        mensagem: "Já existe uma conta com o cpf ou e-mail informado!",
+      });
+    }
+  }
+
+  const conta = contas.find((conta) => {
+    return conta.numero === Number(numeroConta);
+  });
+
+  if (!conta) {
+    return res.status(404).json({ mensagem: "Conta não encontrada." });
+  }
+
+  const encontraUsuario = contas.find((conta) => {
+    return nome === usuario;
+  });
+
+  if (!encontraUsuario) {
+    return res
+      .status(404)
+      .json({ mensagem: "Conta ou usuário não encontrados." });
+  }
+
+  // atualiza os dados
+  conta.nome = nome;
+  conta.cpf = cpf;
+  conta.data_nascimento = data_nascimento;
+  conta.telefone = telefone;
+  conta.email = email;
+  conta.senha = senha;
+
+  return res.status(204).send();
+};
+
+const excluirConta = (req, res) => {
+  const { numeroConta } = req.params;
+
+  // verifica se a conta informada existe
+  const conta = contas.find((conta) => {
+    return conta.numero === Number(numeroConta);
+  });
+
+  if (!conta) {
+    return res.status(404).json({ mensagem: "Conta não encontrada." });
+  }
+
+  // verifica se o saldo é 0
+  if (saldo !== 0) {
+    return res
+      .status(400)
+      .json({ mensagem: "A conta só pode ser removida se o saldo for zero!" });
+  }
+
+  // filtra as contas que não tem o numero passado
+  contas = contas.filter((conta) => {
+    return conta.numero !== Number(numeroConta);
+  });
+
+  return res.status(204).send();
+};
 
 module.exports = {
   listarContas,
